ee87048659b9dd389dcbe5bbf492f5c7062c86a5
placiderapson <placideirandora@gmail.com>
2021-03-06 19:02:18 +0200

    Create a World Time Service for Fetching World Time of  a Specific City

---
+++ lib/services/world_time.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class WorldTime {
+  String time;
+  String flag;
+  String url;
+  String location = '';
+
+  WorldTime({this.location, this.flag, this.url});
+
+  Future<void> getTime() async {
+    http.Response response =
+        await http.get(Uri.http('worldtimeapi.org', 'api/timezone/$url'));
+
+    Map data = jsonDecode(response.body);
+
+    String dateTime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    DateTime now = DateTime.parse(dateTime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    time = now.toString();
+  }
+}
