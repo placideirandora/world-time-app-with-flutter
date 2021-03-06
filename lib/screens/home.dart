9fa70fc592f0c8c4e0d568575bd4f30ab02c22d4
placiderapson <placideirandora@gmail.com>
2021-03-06 22:44:18 +0200

    Display Day and Night Graphics  and Colors Accordingly

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,42 +14,60 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     data = ModalRoute.of(context).settings.arguments;
 
+    String bgImage = data['isDayTime'] ? 'images/day.jpg' : 'images/night.jpg';
+    Color color = data['isDayTime'] ? Colors.black : Colors.white;
+    Color barColor =
+        data['isDayTime'] ? Colors.yellowAccent[100] : Colors.pinkAccent[100];
+
     return Scaffold(
+      backgroundColor: barColor,
       body: SafeArea(
-          child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 50),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            OutlinedButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text('Edit Location')),
-            SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    data['location'],
-                    style: TextStyle(fontSize: 28, letterSpacing: 3),
-                  )
-                ],
+          child: Container(
+        decoration: BoxDecoration(
+            image:
+                DecorationImage(image: AssetImage(bgImage), fit: BoxFit.cover)),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 50),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              OutlinedButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: Icon(
+                    Icons.edit_location,
+                    color: color,
+                  ),
+                  label: Text(
+                    'Edit Location',
+                    style: TextStyle(color: color),
+                  )),
+              SizedBox(
+                height: 10,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              data['time'],
-              style: TextStyle(fontSize: 50),
-            )
-          ],
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                          fontSize: 28, letterSpacing: 3, color: color),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                data['time'],
+                style: TextStyle(fontSize: 50, color: color),
+              )
+            ],
+          ),
         ),
       )),
     );
