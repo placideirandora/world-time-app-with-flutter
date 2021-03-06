b6ae3ed01cdc96c3fd5307b2186b04bc42b751af
placiderapson <placideirandora@gmail.com>
2021-03-06 19:03:06 +0200

    Refactor and Reuse the World Time Service and Dispaly the Time State on the Loading Screen Respectively

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class LoadingScreen extends StatefulWidget {
   LoadingScreen({Key key}) : super(key: key);
@@ -10,29 +9,34 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  String time = 'Loading...';
+
+  void setupWorldTime() async {
+    WorldTime instance =
+        WorldTime(location: 'Paris', flag: 'paris.png', url: 'Europe/paris');
+
+    await instance.getTime();
+
+    setState(() {
+      time = instance.time;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
 
-    getTime();
-  }
-
-  void getTime() async {
-    http.Response response = await http
-        .get(Uri.http('worldtimeapi.org', 'api/timezone/Africa/Kigali'));
-
-    Map data = jsonDecode(response.body);
-
-    String dateTime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    DateTime now = DateTime.parse(dateTime);
-    now = now.add(Duration(hours: int.parse(offset)));
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading Screen'),
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Text(time),
+      )),
     );
   }
 }
