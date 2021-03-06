0a869e1a46a84633b38ad6e6b6c3e1591a63fc00
placiderapson <placideirandora@gmail.com>
2021-03-06 09:02:10 +0200

    Setup the Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:WorldTime/screens/home.dart';
+import 'package:WorldTime/screens/choose_location.dart';
+import 'package:WorldTime/screens/loading.dart';
 
-void main() => runApp(MaterialApp(home: HomeScreen()));
+void main() => runApp(MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => LoadingScreen(),
+        '/home': (context) => HomeScreen(),
+        '/location': (context) => ChooseLocationScreen()
+      },
+    ));
