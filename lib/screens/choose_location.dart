478e3747938735a89e3f21547d954b9e600c78eb
placiderapson <placideirandora@gmail.com>
2021-03-06 04:35:24 +0200

    Create the Choose Location Screen

---
+++ lib/screens/choose_location.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocationScreen extends StatefulWidget {
+  ChooseLocationScreen({Key key}) : super(key: key);
+
+  @override
+  _ChooseLocationScreenState createState() => _ChooseLocationScreenState();
+}
+
+class _ChooseLocationScreenState extends State<ChooseLocationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Choose Location Screen'),
+    );
+  }
+}
